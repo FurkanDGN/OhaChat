f5db5724a2f6a70f216cf4da5503aeb860ccd8ee
Furkan Doğan <furkanaxx34@gmail.com>
2022-05-08 17:56:23 +0300

    initial commit

---
+++ src/main/java/com/outlook/furkan/dogan/dev/ohachat/OhaChat.java
@@ -0,0 +1,17 @@
+package com.outlook.furkan.dogan.dev.ohachat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("unused")
+public final class OhaChat extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+  }
+
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+}
